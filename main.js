e33c1e9594fe1a82bc930de62dfc46467f9c9270
BoskiMarecki <72198070+BoskiMarecki@users.noreply.github.com>
2021-01-04 15:30:07 +0100

    UP
    UP

--- main.js
+++ main.js
@@ -1,102 +1,20 @@
-class Note {
-    constructor (title, content, color, pinned = false) {
-        this.title = title;
-        this.content = content;
-        this.color = color;
-        this.pinned = pinned;
-        this.createDate = new Date();
-        this.id = Date.now();
-    }
+function lz(i) {
+    return `${i}`.padStart(2, "0");
 }
 
-class Notes {
-    constructor (containerSelector) {
-        this.note = new Note();
-        this.notes = [];
-        this.db = new Db();
-        this.notesUI = new NotesUI(containerSelector);
-    }
+const elDate = document.querySelector("#Date")
+const elTime = document.querySelector("#Time")
 
-    addNote(note) {
-        this.notes.push(note);
-        this.db.saveNotes(this.notes);
-        this.notesUI.addNote(note);
-        this.notesUI.clearNoteTitleAndContent(note);
-    }
-    removeNote(id) {
-        this.notes = this.notes.filter(el => el.id !== id);
+function showTextTime() {
+    const now = new Date();
+    const days = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"];
 
-        this.db.saveNotes(this.notes);
-        this.notesUI.removeNote(id);
-    }
-    getNote(id) {
-        return this.notes.find(el => el.id === id);
-    } 
-    getNotes() {
-        return [...this.notes];
-    }
+    const textDate = `${lz(now.getDate())} . ${lz((now.getMonth()+1))} . ${now.getFullYear()} (${days[now.getDay()]})`;
+    const textTime = `${lz(now.getHours())} : ${lz(now.getMinutes())} : ${lz(now.getSeconds())}`;
+
+    elDate.innerHTML = textDate;
+    elTime.innerHTML = textTime;
+
+    window.requestAnimationFrame(showTextTime);
 }
-
-class NotesUI {
-    constructor(containerSelector = 'main') {
-        this.notes = new Notes();
-        this.notesContainer = document.querySelector(containerSelector);
-    }
-
-    addNote(note) {
-        const htmlNote = this.createNote(note);
-        const container = this.getNotesContainer();
-        container.appendChild(htmlNote);
-    }
-    createNote(note) {
-        const htmlNote = document.createElement('section');
-        const htmlTitle = document.createElement('h3');
-        const htmlContent = document.createElement('p');
-        const htmlTime = document.createElement('time');
-        const htmlButton = document.createElement('button');
-
-        htmlNote.classList.add('note');
-        htmlTitle.innerHTML = note.title;
-        htmlContent.innerHTML = note.content;
-        htmlTime.innerHTML = note.createDate.toLocaleString();
-        htmlButton.innerHTML = 'Remove';
-        htmlButton.classList.add('removeBtn');
-
-        
-        htmlNote.appendChild(htmlTitle);
-        htmlNote.appendChild(htmlContent);
-        htmlNote.appendChild(htmlTime);
-        htmlNote.appendChild(htmlButton);
-    }
-    removeNote(id) {
-        const note = this.getNote(id);
-        const container = this.getNotesContainer();
-        container.removeChild(note);
-    }
-    getNote(id) {
-        return document.querySelector('#' + id);
-    }
-    getNotesContainer() {
-        return this.notesContainer;
-    }
-    clearNoteTitleAndContent(){
-        document.querySelector('#noteTitle').value = '';
-        document.querySelector('#noteContent').value = '';
-    }
-}
-
-class Db {
-    constructor() {
-        this.lsNotesKey = 'notes';
-    }
-
-    saveNotes(notes) {
-        localStorage.setItem(this.lsNotesKey, JSON.stringify(notes));
-    }
-    getNotes() {
-        if (localStorage.getItem(this.lsNotesKey) != null) {
-            return JSON.parse(localStorage.getItem(this.lsNotesKey));
-        }
-
-    }
-}
+window.requestAnimationFrame(showTextTime);
