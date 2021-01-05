5ce9b81fb253d32bc8ea63821d7191f13a9be452
BoskiMarecki <72198070+BoskiMarecki@users.noreply.github.com>
2021-01-05 11:49:27 +0100

    ver Beta
    Beta

--- main.js
+++ main.js
@@ -18,3 +18,53 @@ function showTextTime() {
     window.requestAnimationFrame(showTextTime);
 }
 window.requestAnimationFrame(showTextTime);
+
+const localStorageNotesKey = 'notes';
+const notes = [];
+
+document.querySelector('#noteAdd').addEventListener('click', onNewNote)
+
+function onNewNote() {
+    const title = document.querySelector('#noteTitle').value;
+    const content = document.querySelector('#noteContent').value;
+    const note = {
+        title: title,
+        content: content,
+        createDate: new Date(),
+    };
+    
+
+notes.push(note);
+console.log(note);
+
+localStorage.setItem(localStorageNotesKey, JSON.stringify(notes));
+
+const notesFromStorage = JSON.parse(localStorage.getItem(localStorageNotesKey));
+
+const converted = notesFromStorage.map( note => {
+    note.createDate = new Date(note.createDate);
+    return note;
+});
+
+const asidenotes = document.querySelector('aside');
+asidenotes.innerHTML = '';
+
+for (const note of converted) {
+    const htmlSection = document.createElement('section');
+    const htmlTitle = document.createElement('h1');
+    const htmlContent = document.createElement('p');
+    const htmlDate = document.createElement('h4');
+
+    htmlSection.classList.add('note');
+    htmlTitle.innerHTML = note.title;
+    htmlContent.innerHTML = note.content;
+    htmlDate.innerHTML = note.createDate.toLocaleString();
+
+    htmlSection.appendChild(htmlTitle);
+    htmlSection.appendChild(htmlContent);
+    htmlSection.appendChild(htmlDate);
+
+    asidenotes.appendChild(htmlSection);
+    
+}
+}
